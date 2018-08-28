757ccb9bbcafd4b6971bfac9be58ee2cf769be85
Andrew <chowlick@gmail.com>
2018-08-28 15:57:24 -0700

    Initial commit

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nasw17.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SAResources()
+        {
+            ViewBag.Message = "Sexual Assault Resources.";
+
+            return View();
+        }
+
+        public ActionResult SuiResource()
+        {
+            ViewBag.Message = "Suicide Prevention Resources.";
+
+            return View();
+        }
+
+        public ActionResult DVResource()
+        {
+            ViewBag.Message = "Domestic Violence Resources.";
+
+            return View();
+        }
+
+        public ActionResult SAPage()
+        {
+            ViewBag.Message = "Self Report Page";
+
+            return View();
+        }
+
+        public ActionResult Report()
+        {
+            ViewBag.Message = "Report";
+
+            return View();
+        }
+
+        public ActionResult Completion()
+        {
+            ViewBag.Message = "Completion";
+
+            return View();
+        }
+
+        public ActionResult Complete()
+        {
+            ViewBag.Message = "Complete";
+
+            return View();
+        }
+    }
+}
---
+++ Web/Scripts/indexCtrl.js
@@ -0,0 +1,66 @@
+﻿//Index Controller
+(function () {
+    "use strict";
+    angular
+        .module('mainApp')
+        .controller('IndexController', IndexController);
+
+    IndexController.$inject = ['$scope', '$window', '$cookies'];
+
+    function IndexController($scope, $window, $cookies) {
+
+        var vm = this;
+        vm.$scope = $scope;
+        vm.$onInit = _init;
+        vm.$window = $window;
+        vm.$cookies = $cookies;
+        vm.helpBtn = _helpBtn;
+        vm.showBtn = false;
+        vm.categBtn = _categBtn;
+
+        function _init() {
+            console.log("Visits for Suicide: " + $cookies.get('suicide'));
+            console.log("Visits for Sexual Assault: " + $cookies.get('sexual_assault'));
+            console.log("Visits for Domestic Violence: " + $cookies.get('domestic_violence'));
+        }
+        function _helpBtn(type) {
+            if (type == 'me') {
+                vm.showBtn = true;
+            } else {
+                $window.location.href = "/Home/Report";
+            }
+        }
+        function _categBtn(value) {
+            var expireDate = new Date();
+            expireDate.setDate(expireDate.getDate() + 90);
+            if ($cookies.get(value) != null) {
+                var num = $cookies.get(value);
+                num++;
+                $cookies.put(value, num, { 'expires': expireDate });
+            } else {
+                $cookies.put(value, 1, { 'expires': expireDate });
+            }
+            if (value == "suicide") {
+                if ($cookies.get(value) > 4) {
+                    $window.location.href = "/Home/SAPage";
+                } else {
+                    $window.location.href = "/Home/SuiResource";
+                }
+            }
+            if (value == "sexual_assault") {
+                if ($cookies.get(value) > 4) {
+                    $window.location.href = "/Home/SAPage";
+                } else {
+                    $window.location.href = "/Home/SAResources";
+                }
+            }
+            if (value == "domestic_violence") {
+                if ($cookies.get(value) > 4) {
+                    $window.location.href = "/Home/SAPage";
+                } else {
+                    $window.location.href = "/Home/DVResource";
+                }
+            }
+        }
+    }
+})();
